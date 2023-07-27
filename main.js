9f84eeba0b1aa2a08ce1ef7c9cb882cd3da8e8f1
tikembed <140745500+tikembed@users.noreply.github.com>
2023-07-27 16:21:17 -0400

    Update main.js
    Changes to:
    
    Alerts display closer to intended
    Checks for valid URL

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
 function transformUrl() {
     var inputUrl = document.getElementById("inputUrl").value;
-    if(inputUrl.includes("tiktok.com")) {
+    var outputUrl = document.getElementById("outputUrl");
+    var copiedAlert = document.getElementById("copiedAlert");
+    var urlAlert = document.getElementById("urlAlert");
+
+    if(inputUrl.includes("vxtiktok.com")) {
+        outputUrl.value = "";
+        copiedAlert.className = "alert alert-hidden";
+        showUrlAlert("URL already converted!");
+    } else if(inputUrl.includes("tiktok.com")) {
         var newUrl = inputUrl.replace("tiktok.com", "vxtiktok.com");
-        document.getElementById("outputUrl").value = newUrl;
+        outputUrl.value = newUrl;
+        copiedAlert.className = "alert alert-hidden";
+        urlAlert.className = "alert alert-hidden";
     } else {
-        document.getElementById("outputUrl").value = "Invalid TikTok URL";
+        outputUrl.value = "";
+        copiedAlert.className = "alert alert-hidden";
+        showUrlAlert("Invalid TikTok URL or No URL Detected!");
     }
 }
 
 function copyToClipboard() {
     var outputUrl = document.getElementById("outputUrl");
-    outputUrl.select();
-    document.execCommand("copy");
-
     var copiedAlert = document.getElementById("copiedAlert");
-    copiedAlert.className = "alert-visible";
+    var urlAlert = document.getElementById("urlAlert");
+
+    if(outputUrl.value === '' || outputUrl.value === 'Invalid TikTok URL' || outputUrl.value === 'URL already converted!') {
+        showUrlAlert('No URL Detected!');
+    } else {
+        outputUrl.select();
+        document.execCommand("copy");
+        showCopiedAlert();
+    }
+}
+
+function showCopiedAlert() {
+    var copiedAlert = document.getElementById("copiedAlert");
+    copiedAlert.className = "alert alert-visible";
     setTimeout(function() {
-        copiedAlert.className = "alert-hidden";
-    }, 2000); // Hide the alert after 2 seconds
+        copiedAlert.className = "alert alert-hidden";
+    }, 3000); // Hide the alert after 3 seconds
+}
+
+function showUrlAlert(message) {
+    var urlAlert = document.getElementById("urlAlert");
+    urlAlert.textContent = message;
+    urlAlert.className = "alert alert-visible";
+    setTimeout(function() {
+        urlAlert.className = "alert alert-hidden";
+    }, 3000); // Hide the alert after 3 seconds
 }
